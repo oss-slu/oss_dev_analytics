2bb3fc10cddc9cb5fbbb7b4251d1067e61224a78
hcaballero2 <hcaballero0216@gmail.com>
2026-02-08 12:46:42 -0600

    Fix environment variable name for Git token in configs.py and enhance data collection workflow with commit message handling

--- Backend/config/configs.py
+++ Backend/config/configs.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 #Getting API Token to be used in other files
 load_dotenv()
-GIT_TOKEN = os.getenv('GITHUB_TOKEN')
+GIT_TOKEN = os.getenv('GIT_TOKEN')
 
 if not GIT_TOKEN:
     raise ValueError("GIT_TOKEN not found! Please set it in .env file")
