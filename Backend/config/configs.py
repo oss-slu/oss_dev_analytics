0b074a9da764830abbe879981c02cfaf364aa00b
hcaballero2 <hcaballero0216@gmail.com>
2026-02-09 08:18:32 -0600

    Refactor data collection workflow to navigate to Backend directory and update configs.py for local testing; completed local test for automation

--- Backend/config/configs.py
+++ Backend/config/configs.py
@@ -1,8 +1,10 @@
 from configparser import ConfigParser
 import os
 from dotenv import load_dotenv
+from pathlib import Path
 
-
+# having issues finding the .env file when doing local testing, according to my research this should not impact the actions
+env_path = Path(__file__).parent / ".env"
 #Getting API Token to be used in other files
 load_dotenv()
 GIT_TOKEN = os.getenv('GIT_TOKEN')
