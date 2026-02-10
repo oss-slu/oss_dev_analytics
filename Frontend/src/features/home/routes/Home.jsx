cdc9a86afe0739f447b130fc9c72120524746012
Grace Gondela <grace.gondela@slu.edu>
2026-02-10 14:51:21 -0600

    Install vitest and add VolumeChart placeholder to Home page

--- Frontend/src/features/home/routes/Home.jsx
+++ Frontend/src/features/home/routes/Home.jsx
@@ -1,8 +1,12 @@
+import VolumeCharts from "../../../components/charts/VolumeBased";
+
 export const Home = () => {
   return (
     <div>
       <h1>OSS Dev Analytics - Home</h1>
       <p>Welcome to the dashboard.</p>
+
+      <VolumeCharts />
     </div>
   );
 };
