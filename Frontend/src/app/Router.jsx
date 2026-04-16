75fafa985aa8d6de497413604d916f0df5a3adf3
Grace Gondela <grace.gondela@slu.edu>
2026-04-16 11:45:41 -0500

    Fix merge conflict in Router.jsx

--- Frontend/src/app/Router.jsx
+++ Frontend/src/app/Router.jsx
@@ -1,21 +1,6 @@
 import { useRoutes } from 'react-router-dom';
 import { Home } from '../features/home/routes/Home';
 import { TeamStats } from '../features/team-stats/routes/TeamStats';
-<<<<<<< feature/actionable-insights-logic
-
-// Navigation routes for app
-// Wrapping routes with ProtectedRoute so user must be logged in
-
-export const AppRouter = () => {
-  const routes = [
-    { 
-      path: '/', 
-      element: <Home />,
-    },
-    { path: '/team-stats', 
-      element: <TeamStats />,
-    },
-=======
 import Login from "../features/login/routes/Login";
 /*
     Current Routes:
@@ -27,7 +12,6 @@ export const AppRouter = () => {
     { path: "/", element: <Home /> },
     { path: "/team-stats", element: <TeamStats /> },
     { path: "/login", element: <Login /> },
->>>>>>> main
   ];
 
   const element = useRoutes(routes);
