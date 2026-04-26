40e011d015003dfa90112b3a5b83fb68e998913c
hcaballero2 <hcaballero0216@gmail.com>
2026-04-26 15:48:39 -0500

    had issues with viewing NavBar changes on Kiara's Feature/GithubAuthen, pushing to this branch to test/merge

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,13 +1,33 @@
 import { Link } from "react-router-dom";
 import loginProfile from "./loginprofile.png";
+import { useEffect, useState } from "react";
+import { auth } from "../firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 export const Navbar = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe(); 
+  }, []);
+
+  const githubData = user?.providerData?.[0];
+// to collect and return their github username and picture
+  const displayName =
+    githubData?.displayName ||
+    user?.reloadUserInfo?.screenName ||
+    "GitHub User";
+
+  const photoURL = githubData?.photoURL || loginProfile;
+
   return (
     <div style={styles.wrapper}>
       <nav style={styles.nav}>
         <div style={styles.logoContainer}>
-          {" "}
-          {/* OSS logo*/}
           <div style={styles.logoSmall}>
             Open
             <br />
@@ -19,29 +39,34 @@ export const Navbar = () => {
         </div>
 
         <ul style={styles.navLinks}>
-          {" "}
-          {/*Navigation links to components*/}
           <li>
             <Link to="/" style={styles.link}>
               Home
             </Link>
           </li>
+
           <li>
             <Link to="/team-stats" style={styles.link}>
               Team
             </Link>
           </li>
-          <li>
-            <Link to="/login" style={styles.link}>
-              <img src={loginProfile} alt="Profile" style={styles.profileImg} />
+        
+          <li style={{ textAlign: "center" }}>
+            <Link to={user ? "/" : "/login"} style={styles.link}> 
+              <img src={photoURL} alt="Profile" style={styles.profileImg} />
             </Link>
+
+            {user && (
+              <div style={styles.userInfo}>
+                <div style={styles.username}>{displayName}</div>
+              </div>
+            )}
           </li>
         </ul>
       </nav>
     </div>
   );
 };
-
 
 const styles = {
   wrapper: {
@@ -83,7 +108,7 @@ const styles = {
     margin: 0,
     padding: 0,
     fontSize: "1.2rem",
-    alignItems: "center", 
+    alignItems: "center",
   },
   link: {
     color: "white",
@@ -94,6 +119,19 @@ const styles = {
     height: "40px",
     objectFit: "cover",
     cursor: "pointer",
+    borderRadius: "50%",
+  },
+  signedInText: {
+    fontSize: "12px",
+    marginTop: "5px",
+    color: "#A8D0FF",
+  },
+  username: {
+    fontSize: "12px",
+    color: "#A8D0FF",
+    maxWidth: "80px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    whiteSpace: "nowrap",
   },
 };
-
